66020ed42cc886b608aa7bd5d9334ef8f3891709
santisan1 <108411999+santisan1@users.noreply.github.com>
2026-01-15 17:16:51 -0300

    trigger vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1401,7 +1401,7 @@ const UnassignedModal = ({ acampantes, habitaciones, onSelectAcampante, onAssign
                         <span className="text-xs text-slate-500 font-semibold">DNI: {acampante.dni}</span>
                         <span className="text-xs text-slate-300">•</span>
                         <span className="text-xs text-slate-500 font-semibold">
-                          {acampante.sexo === 'Hombre' ? 'MALE' : 'FEMALE'}
+                          {acampante.sexo === 'Masculino' ? 'MALE' : 'FEMALE'}
                         </span>
                         <span className="text-xs text-slate-300">•</span>
                         <span className="text-xs text-slate-500 font-semibold">
